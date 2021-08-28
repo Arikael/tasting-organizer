50eaa44eb287e609f6828bd84440327f734ccddd
Arikael <mhaeberle@mailbox.org>
2021-08-28 14:06:36 +0200

    fix publicPath for prod

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 module.exports = {
-  pluginOptions: {
-    quasar: {
-      importStrategy: 'kebab',
-      rtlSupport: false
-    }
-  },
-  transpileDependencies: [
-    'quasar'
-  ]
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/tasting-organizer/'
+        : '/',
+    pluginOptions: {
+        quasar: {
+            importStrategy: 'kebab',
+            rtlSupport: false
+        }
+    },
+    transpileDependencies: [
+        'quasar'
+    ]
 }
