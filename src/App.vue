b8f17f0a19f8b6c337bfd8085bf5c1e6623b1325
Arikael <mhaeberle@mailbox.org>
2021-09-24 14:17:07 +0200

    change quasar language too (needs fix to initialize quasar with the correct language)

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
                 <q-item clickable>
                   <q-item-section v-on:click="changeLanguage('de')">DE</q-item-section></q-item>
                 <q-item clickable>
-                  <q-item-section v-on:click="changeLanguage('en')">EN</q-item-section>
+                  <q-item-section v-on:click="changeLanguage('en-US')">EN</q-item-section>
                 </q-item>
               </q-list>
             </q-menu>
@@ -27,9 +27,9 @@
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue';
-
+import {defineComponent, getCurrentInstance, ref, watch} from 'vue';
 import Results from './components/Results.vue'
+import {useQuasar} from "quasar";
 
 export default defineComponent({
   name: 'LayoutDefault',
@@ -38,9 +38,33 @@ export default defineComponent({
     Results
   },
 
+  setup () {
+    const $q = useQuasar()
+    const lang = ref($q.lang.isoName)
+    const i18n = getCurrentInstance().appContext.config.globalProperties.$i18n
+
+    watch(lang, val => {
+      i18n.locale = val.toLowerCase().split('-')[0]
+      console.log('test')
+      // dynamic import, so loading on demand only
+      import(
+          /* webpackInclude: /(de|en-US)\.js$/ */
+      'quasar/lang/' + val
+          ).then(lang => {
+        $q.lang.set(lang.default)
+      })
+    })
+
+    return {
+      lang
+    }
+  },
+  mounted() {
+    this.lang = navigator.language
+  },
   methods: {
     changeLanguage(key: string) {
-      this.$i18n.locale = key.toLowerCase()
+      this.lang = key
     }
   }
 });
