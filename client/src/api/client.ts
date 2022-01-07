7914527cc6f4ea23c845ca3a70df59e7c6f0524f
Arikael <mhaeberle@mailbox.org>
2022-01-07 14:36:58 +0100

    fix import

--- client/src/api/client.ts
+++ client/src/api/client.ts
@@ -1,7 +1,7 @@
 import io from "socket.io-client"
 import feathers from "@feathersjs/feathers"
 import socketio from "@feathersjs/socketio-client"
-import {ServiceTypes} from '../../../server/types'
+import {ServiceTypes} from './types'
 
 export function createClient(): feathers.Application<ServiceTypes> {
     const socket = io(process.env.VUE_APP_BACKEND_URL as string);
