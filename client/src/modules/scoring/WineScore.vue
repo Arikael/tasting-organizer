30bed60f78560f21072875d5b0712febf5f92350
Arikael <mhaeberle@mailbox.org>
2022-04-08 18:17:06 +0200

    added winescoring functions to use useScoringValidators.ts

--- client/src/modules/scoring/WineScore.vue
+++ client/src/modules/scoring/WineScore.vue
@@ -8,12 +8,12 @@
 </template>
 
 <script lang="ts">
-import {computed, defineComponent, onMounted, ref, WritableComputedRef} from 'vue';
-import {BaseWineDto, ScoringScaleItem, UserScoresDto} from '@/api/types'
+import {computed, defineComponent, onMounted} from 'vue';
+import {BaseWineDto, ScoringScaleItem} from '@/api/types'
 import {QInput} from 'quasar';
 import {store} from "@/store";
 import {useI18n} from "vue-i18n";
-import {FormFieldState} from "@/common/types";
+import {useScoringForWine} from "@/modules/scoring/useScoringValidators";
 
 export default defineComponent({
   name: 'WineScore',
@@ -24,54 +24,18 @@ export default defineComponent({
   },
   setup(props) {
     const i18n = useI18n({useScope: 'global'})
-    const scoreStatus = ref<FormFieldState<number>>(new FormFieldState<number>(0))
+    const scoringForWine = useScoringForWine(props.wine?.id ?? '')
 
-    const setLocalScoreFromState = (wineId: string, defaultValue = 0) => {
-      if (wineId) {
-        scoreStatus.value.value = store.getters.getScore(wineId)?.score ?? defaultValue
-      }
-    }
 
     onMounted(() => {
-      setLocalScoreFromState(props.wine?.id ?? '')
+      scoringForWine.setLocalScoreFromState(props.wine?.id ?? '')
     })
 
-    const setScore = (value: number) => {
-      const score = parseInt(value.toString(10), 10)
-      const wineId = props.wine?.id ?? ''
-
-      if (!isNaN(score) && wineId) {
-        scoreStatus.value.value = score
-        store.setters.setScore(wineId, score).then((x: boolean | UserScoresDto) => {
-          if (x === false) {
-            scoreStatus.value.ok = false
-          } else {
-            scoreStatus.value.ok = true
-
-            setLocalScoreFromState(wineId, score)
-          }
-        })
-      } else {
-        scoreStatus.value.ok = false
-      }
-    }
-
-    const score: WritableComputedRef<number> = computed({
-          get(): number {
-            return scoreStatus.value.value
-          },
-          set(value: number): void {
-            setScore(value)
-          }
-        }
-    )
 
     return {
       store,
-      currentScale: store.getters.currentScoreScale,
-      scoreStatus: computed(() => scoreStatus.value),
+      ...scoringForWine,
       wineName: props.wine?.name ?? '',
-      score,
       label: computed((): string => {
         if (store.state.tasting.revealAfter !== 'always') {
           return `${i18n.t('Wine')} ${(props.wine?.name ?? '')}`
@@ -81,7 +45,7 @@ export default defineComponent({
       }),
       displayScaleDescription: computed(() => {
         const scaleItem = store.getters.currentScoreScale.value.items.find((x: ScoringScaleItem) => {
-          return x.min <= scoreStatus.value.value && scoreStatus.value.value <= x.max
+          return x.min <= scoringForWine.scoreStatus.value.value && scoringForWine.scoreStatus.value.value <= x.max
         })
 
         if (scaleItem) {
