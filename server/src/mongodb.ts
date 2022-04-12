31b1ba8d25d3c11f7e4b236ee8ffae44e2416e8f
Arikael <mhaeberle@mailbox.org>
2022-04-12 21:37:01 +0200

    add some debugging

--- server/src/mongodb.ts
+++ server/src/mongodb.ts
@@ -4,8 +4,10 @@ import { Application } from './declarations'
 export default function (app: Application): void {
   const connection = app.get('mongodb')
   const database = connection.substr(connection.lastIndexOf('/') + 1)
+  console.log('connection:' + connection)
   const mongoClient = MongoClient.connect(connection)
     .then(client => client.db(database))
+    .catch(err => console.log(err))
 
   app.set('mongoClient', mongoClient)
 }
