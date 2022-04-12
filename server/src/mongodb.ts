505c8ab9f23a9d1ad9868d8931d18b7c7de98e1d
Arikael <mhaeberle@mailbox.org>
2022-04-12 23:02:25 +0200

    add retrywrites and w

--- server/src/mongodb.ts
+++ server/src/mongodb.ts
@@ -4,8 +4,11 @@ import { Application } from './declarations'
 export default function (app: Application): void {
   const connection = app.get('mongodb')
   const database = connection.substr(connection.lastIndexOf('/') + 1)
-  console.log('connection=' + connection)
-  const mongoClient = MongoClient.connect(connection)
+
+  const mongoClient = MongoClient.connect(connection, {
+    retryWrites: true,
+    w: 'majority'
+  })
     .then(client => client.db(database))
     .catch(err => console.log(err))
 
